cc564273d1247f0fbfd797dc60bc6bb97a43e296
robedgar <116982166+robedgar@users.noreply.github.com>
2023-01-29 12:53:56 +0000

    css

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <main>
       <h1>
-        £400<span>.00</span>
+        £900<span>.00</span>
       </h1>
       <form>
         <div className="basic">
@@ -17,7 +17,36 @@ function App() {
         <button type="submit">Add new transaction</button>
       </form>
       <div className="transactions">
-        <div className="transaction"></div>
+        <div className="transaction">
+          <div className="left">
+            <div className="name">New Samsung Tv</div>
+            <div className="description">It was time for new Tv</div>
+          </div>
+          <div className="right">
+            <div className="price red">-£600</div>
+            <div className="datetime">09-12-2022 09:45</div>
+          </div>
+        </div>
+        <div className="transaction">
+          <div className="left">
+            <div className="name">New React site</div>
+            <div className="description">Freelance work </div>
+          </div>
+          <div className="right">
+            <div className="price green">+£700</div>
+            <div className="datetime">02-11-2022 15:45</div>
+          </div>
+        </div>
+        <div className="transaction">
+          <div className="left">
+            <div className="name">Iphone</div>
+            <div className="description">Phone upgrade</div>
+          </div>
+          <div className="right">
+            <div className="price red">£900</div>
+            <div className="datetime">12-12-2022 13:45</div>
+          </div>
+        </div>
       </div>
     </main>
   );
